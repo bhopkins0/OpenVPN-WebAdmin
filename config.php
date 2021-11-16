80617402710b8dacb378af9b5e7e9fe2e55ccf07
Brent Hopkins <contact@brenthopkins.me>
2021-11-15 18:04:21 -0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = ""; // MySQL servername
+$username = ""; // MySQL username
+$password = ""; // MySQL password
+$dbname = ""; // MySQL database containing the 'config' table 
+$pw = ''; // Password to access the WebAdmin
+$vpnserver = ""; // IP address of the OpenVPN server
+$vpnserveruser = ""; // SSH username of the OpenVPN server
+$vpnserverpw = ''; // SSH password of the OpenVPN server
+$restrict2vpn = "0"; // Change this to "1" if you want the webadmin to only be accessible while connected to the VPN server
+
+if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+  $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+}
