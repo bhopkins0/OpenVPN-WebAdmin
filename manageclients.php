bbb95762b7c65a892cffa6d80cf6a122afe03601
Brent Hopkins <contact@brenthopkins.me>
2022-03-29 13:17:14 -0500

    Update manageclients.php

--- manageclients.php
+++ manageclients.php
@@ -3,7 +3,6 @@ session_start();
 include 'config.php';
 include 'Net/SSH2.php';
 
-
 if ($restrict2vpn == "1") {
     if ($_SERVER["REMOTE_ADDR"] !== $vpnserver) {
         echo <<<EOF
@@ -12,13 +11,67 @@ if ($restrict2vpn == "1") {
         die();
     }
 }
-
-if ($_SESSION["auth"] !== "1") {
-        header('Location: /');
-        die();
+if ($_SESSION["auth"] != "1") {
+header("Location: index.php");
+die();
 }
 
-// User wants to revoke a client
+
+if (isset($_POST["commonname"]) && $_SESSION["auth"] == "1" && $_POST["key"] == $_SESSION["key"]) {
+    $commonname = strtolower($_POST["commonname"]);
+
+    // CN is not alphanumeric and is greater than 32
+    if (strlen($commonname) > 32 || !preg_match("/^[A-Za-z0-9]*$/", $commonname)) {
+        $_SESSION["err"] = 0;
+        header("Location: manageclients.php");
+        die();
+    }
+
+    // CN is blank
+    if (strlen($commonname) < 1) {
+        $_SESSION["err"] = 1;
+        header("Location: manageclients.php");
+        die();
+    }
+    $conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
+    if (!$conn) {
+        die("An error occurred.");
+    }
+    $sql = "SELECT cn FROM config WHERE cn='$commonname' AND status='active'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        // The CN is already in use
+        $_SESSION["err"] = 2;
+        header("Location: manageclients.php");
+        die();
+    }
+    mysqli_close($conn);
+
+    $conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
+    if (!$conn) {
+        die("Error");
+    }
+    $sql = "INSERT INTO config (cn, status)
+    VALUES ('$commonname', 'active')";
+    if (mysqli_query($conn, $sql)) {
+
+    } else {
+        die("Error");
+    }
+
+    $ssh = new Net_SSH2($vpnserver);
+    if (!$ssh->login($vpnserveruser, $vpnserverpw)) {
+        exit('Error');
+    }
+
+    // Run the create.sh script on the server and download the new .ovpn file
+    $ssh->exec('/var/openvpn_scripts/create.sh '.$commonname);
+    header("Location: manageclients.php");
+    die();
+}
+
+
+
 if (isset($_GET["delconfig"]) && $_SESSION["auth"] == "1" && $_GET["key"] == $_SESSION["key"]) {
     $commonname = $_GET["delconfig"];
     $conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
@@ -84,57 +137,92 @@ if (isset($_GET["dl"]) && $_SESSION["auth"] == "1" && $_GET["key"] == $_SESSION[
 ?>
 
 <html>
-    <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VPN Admin - Manage Clients</title>
-    <link rel="stylesheet" href="bootstrap.min.css">
-    <style>
-    html,
-    body {
-      height: 100%;
-    }
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Client Manager</title>
+<link rel="stylesheet" href="bootstrap.min.css">
+</head>
+<body class="bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarToggler">
+      <a class="navbar-brand" href="#">VPN Admin</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="manageclients.php">Client Manager</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="vpnmanager.php">VPN Manager</a>
+        </li>
 
-    body {
-      display: flex;
-      align-items: center;
-      padding-top: 40px;
-      padding-bottom: 40px;
-      background-color: #f5f5f5;
-    }
+      </ul>
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="accmanager.php">WebAdmin Manager</a>
+        </li>
+      </ul>
 
-    .vpn {
-      width: 100%;
-      max-width: 500px;
-      margin: auto;
-      padding: 15px;
-    }
-
-
-    </style>
-    </head>
-    <body>
-        <main class="vpn">
-        <h1 class="display-4">Manage Clients</h1>
-     <div>
+    </div>
+  </div>
+</nav>
 <?php
-if ($_SESSION["auth"] == "1") {
-    $key = $_SESSION["key"];
-    if ($_SESSION["err"] == 1) {
-        echo '<p class="lead text-danger">Unable to delete configuration: Common name not found in database</p>';
-        $_SESSION["err"] = 0;
-    }
-    if ($_SESSION["err"] == 2) {
-        echo '<p class="lead text-danger">Unable to delete from database. Client may have been revoked.</p>';
-        $_SESSION["err"] = 0;
-    }
-    if ($_SESSION["err"] == 3) {
-        echo '<p class="lead text-danger">Error downloading the OpenVPN configuration..</p>';
-        $_SESSION["err"] = 0;
-    }
-
+if ($_SESSION["err"] == 0) {
 echo <<<EOL
 
-<table class="table">
+<div class="mt-4 text-white d-flex align-items-center justify-content-center">
+<div class="alert alert-danger" role="alert">
+CN must be alphanumeric and less than 32 characters
+</div>
+</div>
+EOL;
+
+$_SESSION["err"] = -1;
+}
+if ($_SESSION["err"] == 1) {
+echo <<<EOL
+
+<div class="mt-4 text-white d-flex align-items-center justify-content-center">
+<div class="alert alert-danger" role="alert">
+CN must not be blank
+</div>
+</div>
+EOL;
+
+$_SESSION["err"] = -1;
+}
+if ($_SESSION["err"] == 2) {
+echo <<<EOL
+
+<div class="mt-4 text-white d-flex align-items-center justify-content-center">
+<div class="alert alert-danger" role="alert">
+CN is already in use
+</div>
+</div>
+EOL;
+
+$_SESSION["err"] = -1;
+}
+?>
+<div class="mt-4 text-white d-flex align-items-center justify-content-center">
+<div>
+<h4 class="text-center">Create new client</h4>
+<form class="form-inline" method="post">
+<div class="form-group mb-3">
+<input type="text" class="form-control" name="commonname" placeholder="CN (Common Name)">
+<input type="hidden" id="key" name="key" value="<?php echo $_SESSION['key']; ?>">
+</div>
+<input class="btn btn-primary w-100" type="submit" value="Generate Configuration">
+</div>
+</div>
+<div class="mt-4 text-white d-flex align-items-center justify-content-center">
+<div class="table-responsive">
+<table class="table table-dark">
   <thead>
     <tr>
       <th scope="col">#</th>
@@ -144,7 +232,8 @@ echo <<<EOL
     </tr>
   </thead>
   <tbody>
-EOL;
+<?php
+
 $conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
 if (!$conn) {
   die("Error occured...");
@@ -156,21 +245,28 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
     $key = $_SESSION["key"];
     echo '<tr><th scope="row">'.$incvar.'</th><td>'. $row["cn"] .'</td><td><a href="manageclients.php?dl='.$row["cn"].'&key='.$key.'" class="btn btn-primary">Download</a></td><td><a href="manageclients.php?delconfig='.$row["cn"].'&key='.$key.'" class="btn btn-danger">Delete</a></td></tr>';
-    $incvar=$incvar+1;
+    $incvar++;
   }
 }
 $conn->close();
 
-echo <<<EOF
+
+?>
   </tbody>
 </table>
-            </div>
-<a class="btn btn-outline-danger w-100" href="/">Back to Homepage</a>
-        </main>
-    </body>
+</div></div>
+<?php
+$key = $_SESSION["key"];
+echo <<<EOL
+<div class="p-4 mt-2 d-flex align-items-center justify-content-center">
+<form method="post" action="index.php" class="col-sm-9 col-md-6 col-lg-8">
+<input type="hidden" id="lo" name="lo" value="$key">
+<input class="btn btn-outline-danger w-100" type="submit" value="Logout">
+</form>
+</div>
+<div class="mt-4 text-white d-flex align-items-center justify-content-center">
+<div>
+EOL;?>
+<script src="bootstrap.bundle.min.js"></script>
+</body>
 </html>
-
-EOF;
-die();
-}
-?>
